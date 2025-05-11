58a377abcef3a2ae8395131d82971e2f503b2bdb
Andy <andothomas167@gmail.com>
2025-05-11 13:27:09 -0700

    use fastapi

--- server.py
+++ server.py
@@ -1,33 +1,35 @@
-from sanic import Sanic
-from sanic.response import json
-from sanic_ext import Extend
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 import os
-from main import search_images
+from main import search_images, images_path
 
-app = Sanic("server")
+app = FastAPI()
 
-img_dir = os.path.abspath("images")
+origins = [
+  "http://localhost:5173"
+]
 
-@app.get("imgpaths/")
-async def get_image_path(request):
+app.add_middleware(
+  CORSMiddleware,
+  allow_origins=origins,
+  allow_credentials=True,
+  allow_methods=["*"],
+  allow_headers=["*"],
+)
+
+img_dir = images_path
+
+@app.get("/imgpaths")
+async def get_image_path():
   files = os.listdir(img_dir)
-  return json(files)
+  return files
 
-app.static("/images", img_dir)
+app.mount("/images", StaticFiles(directory=img_dir), name="static")
 
 @app.get("/search")
-async def search_handler(request):
-  query = request.args.get("query")
+async def search_handler(query: str, radius: int):
   if not query:
-    return json({"error": "missing 'query'"})
-  results = search_images(query)
-  return json(results)
-
-@app.middleware("response")
-async def add_cors(request, response):
-  response.headers["Access-Control-Allow-Origin"] = "*"
-  response.headers["Access-Control-Allow-Methods"] = "GET, POST, OPTIONS"
-  response.headers["Access-Control-Allow-Headers"] = "Content-Type"
-
-if __name__ == "__main__":
-  app.run(host="localhost", port=8080, debug=True)
+    return {"error": "missing 'query'"}
+  results = search_images(query, top_k=radius)
+  return results
